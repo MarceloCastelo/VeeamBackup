bd625e7c037b14ec4549d53d2cd5c67132ff59c5
Marcelo Castelo <marcelo.cbo2000@gmail.com>
2025-06-06 10:53:07 -0300

    atualização runner + frontend

--- runner.py
+++ runner.py
@@ -1,7 +1,17 @@
 import subprocess
+import schedule
+import time
 
-# Executa o email_processor_db.py
-subprocess.run(['python', 'email_processor_db.py'], check=True)
+def run_scripts():
+    # Executa o email_processor_db.py
+    subprocess.run(['python', 'email_processor_db.py'], check=True)
+    # Inicializa a aplicação Flask (app.py)
+    subprocess.run(['python', 'app.py'], check=True)
 
-# Inicializa a aplicação Flask (app.py)
-subprocess.run(['python', 'app.py'], check=True)
+# Agenda para rodar todos os dias às 7h da manhã
+schedule.every().day.at("07:00").do(run_scripts)
+
+if __name__ == "__main__":
+    while True:
+        schedule.run_pending()
+        time.sleep(60)

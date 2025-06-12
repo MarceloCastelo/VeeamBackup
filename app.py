24dc7f7125659fad87c9fcad5a3f795cb44c59b7
Marcelo Castelo <marcelo.cbo2000@gmail.com>
2025-06-12 14:50:48 -0300

    atualização do schedule.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import threading
 import time
 import os
+import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -24,8 +25,18 @@ def email_checker():
         target_sender=os.environ.get("EMAIL_TARGET_SENDER")
     )
     while True:
+        now = datetime.datetime.now()
+        # Próximos horários: 6h e 12h
+        next_times = [
+            now.replace(hour=6, minute=0, second=0, microsecond=0),
+            now.replace(hour=12, minute=0, second=0, microsecond=0)
+        ]
+        # Se já passou do horário, agenda para o próximo dia
+        next_times = [t if t > now else t + datetime.timedelta(days=1) for t in next_times]
+        next_run = min(next_times)
+        sleep_seconds = (next_run - now).total_seconds()
+        time.sleep(sleep_seconds)
         processor.fetch_and_process()
-        time.sleep(300)  # Verifica a cada 5 minutos
 
 if __name__ == '__main__':
     threading.Thread(target=email_checker, daemon=True).start()
